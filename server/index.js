79d465399140dada2528d690aa7bc057bb190f14
ZerSolo <yosiami15@gmail.com>
2019-03-31 16:02:21 +0200

    add client and server config

---
+++ server/index.js
@@ -0,0 +1,4 @@
+// Purpose of file: To start our HTTP server
+const app = require('./app'); // This loads in the code from app.js
+
+app.listen(8080); // We can use that "app" functionality now in this file to start the server
